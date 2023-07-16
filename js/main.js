6fd4917f42c6f77ac0314fe348faeda9b1f7eaf2
Daniela Rodríguez <danierodga@gmail.com>
2023-07-15 23:06:46 -0600

    Used the 'color' input type to generate a color picker. Included a label for the picked color.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ addEventListener("DOMContentLoaded", DOMLoaded)
 function changeImageSource () {
     let imageSource = document.getElementById("meme_image_url").value
     document.getElementById("meme_image_src").src = imageSource
-    console.log(imageSource)
 }
 
 addEventListener("input", changeImageSource)
