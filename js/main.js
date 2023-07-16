0623507ed3f1d5a1f9d33227b7167d6436c5fb69
Daniela Rodríguez <danierodga@gmail.com>
2023-07-15 22:50:36 -0600

    Added 2 functions: one to charge the default image for the meme (a black image square) and the second one to change the image source when type by keyboard by the user. Added 2 eventListeners so the default image is set when the DOM content is loaded and set the source image when typed by user.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-let imageSource = "./img/meme/background_default.png"
-
-addEventListener("DOMContentLoaded", (event) => {
+function DOMLoaded () {
+    let imageSource = "./img/meme/background_default.png"
     document.getElementById("meme_image_src").src = imageSource
-})
-
-function changeImageSRC (newImageSRC) {
-
 }
+
+addEventListener("DOMContentLoaded", DOMLoaded)
+
+function changeImageSource () {
+    let imageSource = document.getElementById("meme_image_url").value
+    document.getElementById("meme_image_src").src = imageSource
+    console.log(imageSource)
+}
+
+addEventListener("input", changeImageSource)
