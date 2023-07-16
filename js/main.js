d4781536ffec35408c479118affc09be794accf5
Daniela Rodríguez <danierodga@gmail.com>
2023-07-16 03:26:49 -0600

    Added functions to altered the values of the filters

--- js/main.js
+++ js/main.js
@@ -36,11 +36,53 @@ let backgroundColorEffect = document.getElementById("background_meme_color_effec
 backgroundColorEffect.addEventListener("change", updateBackgroundColor)
 
 // Retrieving the array of filters in class "slider"
-// Then, iterating through each element (filter) to check their value and apply it.
 let filters = document.getElementsByClassName("slider")
+
+function updateFiltersValues () {
+    let auxFilter = ""
+    for(var index=0;index < filters.length;index++){
+        let suffixFilter = ""
+        let currentFilter = filters[index]
+        console.log(currentFilter.id,currentFilter.value, currentFilter.innerHTML)
+        if(currentFilter.id === "contrast" || currentFilter.id === "grayscale" || currentFilter.id === "sepia" || currentFilter.id === "saturate")
+            {suffixFilter = "%"}
+        else if(currentFilter.id === "blur")
+            {suffixFilter = "px"}
+        else if(currentFilter.id === "hue-rotation")
+            {suffixFilter = "deg"}
+        
+        if(index == filters.length -1)
+            auxFilter += currentFilter.id+"("+currentFilter.value+suffixFilter+")"
+        else
+            auxFilter += currentFilter.id+"("+currentFilter.value+suffixFilter+") "
+        
+
+    }
+    console.log(auxFilter)
+    document.getElementById("meme_image").style.filter = auxFilter
+}
+
+// Iterating through each element (filter) to check their value and apply it.
 for(var index=0;index < filters.length;index++){
     let currentFilter = filters[index]
-    currentFilter.addEventListener("input", (event) => {
-        document.getElementById("meme_image").style.filter = currentFilter.id+"("+currentFilter.value+")"
-    })
+    currentFilter.addEventListener("change", updateFiltersValues)
 }
+
+function resetFiltersValues () {
+    document.getElementById("brightness").value = "1"
+    document.getElementById("opacity").value = "1"
+    document.getElementById("contrast").value = "100"
+    document.getElementById("blur").value = "0"
+    document.getElementById("grayscale").value = "0"
+    document.getElementById("sepia").value = "0"
+    document.getElementById("hue-rotation").value = "0"
+    document.getElementById("saturation").value = "100"
+    document.getElementById("invert").value = "0"
+}
+
+let filtersReset = document.getElementById("image_filters")
+filtersReset.addEventListener("reset", (event) => {
+    resetFiltersValues()
+    updateFiltersValues()
+    
+})
