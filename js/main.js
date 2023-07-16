8739c080e0065fc8f6cab86a8748a7f1c2309fed
Daniela Rodríguez <danierodga@gmail.com>
2023-07-15 22:23:56 -0600

    Generated the javascript file. Added function to add the source image for the meme once the DOM is loaded.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let imageSource = "./img/meme/background_default.png"
+
+addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("meme_image_src").src = imageSource
+})
+
+function changeImageSRC (newImageSRC) {
+
+}
