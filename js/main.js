4f86478870bbc9aed600a93ba7bd2fbe6f29af14
Daniela Rodríguez <danierodga@gmail.com>
2023-07-16 12:42:52 -0600

    Simplify the document.getElementById using a const

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+const $ = (id) => document.getElementById(id)
+
 function DOMLoaded () {
     let imageSource = "./img/meme/background_default.png"
-    document.getElementById("meme_image").src = imageSource
+    $("meme_image").src = imageSource
 }
 
 addEventListener("DOMContentLoaded", DOMLoaded)
 
 function changeImageSource () {
-    let imageSource = document.getElementById("meme_image_url").value
-    document.getElementById("meme_image").src = imageSource
+    let imageSource = $("meme_image_url").value
+    $("meme_image").src = imageSource
 }
 
-let memeImageURL = document.getElementById("meme_image_url")
+let memeImageURL = $("meme_image_url")
 
 memeImageURL.addEventListener("input", changeImageSource)
 
@@ -20,19 +22,19 @@ memeImageURL.addEventListener("input", changeImageSource)
 // 3. If user selected a background color, update the background color label string for the meme (added as an span in the html doc)
 // 4. Also, if there's any color effect different to 'none' selected, then is gonna apply the new background color and effect to the background of the meme image
 function updateBackgroundColor () {
-    let bgColor = document.getElementById("background_meme_color").value
-    let bgColorEffect = document.getElementById("background_meme_color_effect").value
+    let bgColor = $("background_meme_color").value
+    let bgColorEffect = $("background_meme_color_effect").value
     // Update the label for the background meme color
-    document.getElementById("background_meme_color_label").innerHTML = bgColor
+    $("background_meme_color_label").innerHTML = bgColor
     // Apply (according to the effect selected by the user) the appropiate mix-color-blend css propierty
-    document.getElementById("meme_image_container").style.backgroundColor = bgColor
-    document.getElementById("meme_image").style.mixBlendMode = bgColorEffect    
+    $("meme_image_container").style.backgroundColor = bgColor
+    $("meme_image").style.mixBlendMode = bgColorEffect    
 }
 
-let backgroundColor = document.getElementById("background_meme_color")
+let backgroundColor = $("background_meme_color")
 backgroundColor.addEventListener("change", updateBackgroundColor)
 
-let backgroundColorEffect = document.getElementById("background_meme_color_effect")
+let backgroundColorEffect = $("background_meme_color_effect")
 backgroundColorEffect.addEventListener("change", updateBackgroundColor)
 
 // Retrieving the array of filters in class "slider"
@@ -59,7 +61,7 @@ function updateFiltersValues () {
 
     }
     console.log(auxFilter)
-    document.getElementById("meme_image").style.filter = auxFilter
+    $("meme_image").style.filter = auxFilter
 }
 
 // Iterating through each element (filter) to check their value and apply it.
@@ -69,18 +71,18 @@ for(var index=0;index < filters.length;index++){
 }
 
 function resetFiltersValues () {
-    document.getElementById("brightness").value = "1"
-    document.getElementById("opacity").value = "1"
-    document.getElementById("contrast").value = "100"
-    document.getElementById("blur").value = "0"
-    document.getElementById("grayscale").value = "0"
-    document.getElementById("sepia").value = "0"
-    document.getElementById("hue-rotate").value = "0"
-    document.getElementById("saturate").value = "100"
-    document.getElementById("invert").value = "0"
+    $("brightness").value = "1"
+    $("opacity").value = "1"
+    $("contrast").value = "100"
+    $("blur").value = "0"
+    $("grayscale").value = "0"
+    $("sepia").value = "0"
+    $("hue-rotate").value = "0"
+    $("saturate").value = "100"
+    $("invert").value = "0"
 }
 
-let filtersReset = document.getElementById("image_filters")
+let filtersReset = $("image_filters")
 filtersReset.addEventListener("reset", (event) => {
     resetFiltersValues()
     updateFiltersValues()
